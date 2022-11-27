9950876eb22d0494af09afffaf8597e4574927b0
CSMRCK <42943919+CSMRCK@users.noreply.github.com>
2022-11-27 21:12:32 +0200

    cleaned up program file

--- API/Program.cs
+++ API/Program.cs
@@ -7,16 +7,6 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
